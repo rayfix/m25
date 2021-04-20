b48738d4987cbe968ca78999fc89ee90a7f9002a
Ray Fix <rayfix@gmail.com>
2021-04-20 13:49:46 -0700

    add better help and copyright

--- m25/main.swift
+++ m25/main.swift
@@ -1,8 +1,21 @@
+// Copyright 2021 Ray Fix
 //
-//  main.swift
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify,
+// merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to the
+// following conditions:
 //
-//  Created by Ray Fix on 4/19/21.
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
 //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import Foundation
 import ArgumentParser
@@ -16,19 +29,41 @@ struct Multiplication {
 struct RunQuiz: ParsableCommand {
 
   static var configuration: CommandConfiguration {
-    CommandConfiguration(commandName: "m25")
+    CommandConfiguration(commandName: "m25",
+                         discussion: """
+        Practice multiplication tables 25x25
+
+        Examples:
+           # Practice 12x2 up to 12x25 scrambled.
+           m25 12
+
+           # Practice 12x2 up to 12x25, present questions in order.
+           m25 --order 12
+
+           # Practice 12, 13, and 14
+           m25 12 13 14
+
+           # Practice 15, 16 but limit to 10 questions.
+           m25 15 16 --limit 10
+
+           # Show 12x2 to 12x25 in order and exit.
+           m25 12 --show --order
+
+           # Show 12x12 to 12x24 in order and exit
+           m25 12 --show --order --from 12 --to 24
+      """)
   }
 
   @Argument(help: "Specify the numbers to study.")
   var numbersToStudy: [Int]
 
-  @Option(name: .shortAndLong)
+  @Option(name: .shortAndLong, help: "Limit the number of questions.")
   var limit: Int?
 
-  @Option(name: .shortAndLong)
+  @Option(name: .shortAndLong, help: "The start of the multiplication table.")
   var from: Int = 2
 
-  @Option(name: .shortAndLong)
+  @Option(name: .shortAndLong, help: "The end of the multiplication table.")
   var to: Int = 25
 
   @Flag(name: .customLong("order"), help: "Do not shuffle the questions.")
